509cdc660d8e0e31cf0fbc57d1bf96aabb43bc4e
Dana Da Costa <dana.dacosta.001@student.uni.lu>
2024-01-03 20:51:56 +0100

    Clients_dana

--- client.py
+++ client.py
@@ -1,12 +1,16 @@
 import random
 from socket import *
+import threading
 
 class UnreliableReceiver:
-    def __init__(self, socket, ack_probability):
-        self.socket = socket
+    def __init__(self, socket, ack_probability, total_clients):
+        self.socket = socket #socket(AF_INET, SOCK_DGRAM)
         self.ack_probability = ack_probability
         self.total_bytes_received = 0
         self.retransmissions_received = 0
+        self.total_clients = total_clients
+        self.clients_finished = 0
+        self.socket_closed = False
         
 
     def unreliable_send_ack(self, packet_id, server_address):
@@ -20,45 +24,79 @@ class UnreliableReceiver:
         
         return True
 
-    def receive_data(self):
-        while True:
-            modified_message, server_address = self.socket.recvfrom(2048)
+    def receive_data(self, client_id):
+        try:
+            while True:
+                modified_message, server_address = self.socket.recvfrom(2048)
 
-            if modified_message == b'finished':
-                print("Transfer finished")
-                break
+                if modified_message == b'finished':
+                    print("Transfer finished")
+                    self.clients_finished += 1
+                    if self.clients_finished == self.total_clients:
+                        self.close_socket()
+                        break
 
-            modified_message.decode()
+                    continue
+
+                modified_message.decode()
+                
+                packet_id1 = modified_message[:6]
+                packet_id2 = int(packet_id1)
             
-            packet_id1 = modified_message[:6]
-        
+                
+                #data = modified_message[6:]
+                print(f"Received packet with ID: {packet_id1}")
+
+                if not self.unreliable_send_ack(packet_id1, server_address):
+                    print("Retransmission received")
+                    self.retransmissions_received += 1
+                self.total_bytes_received += len(modified_message)
             
-            #data = modified_message[6:]
-            print(f"Received packet with ID: {packet_id1}")
+        except OSError as e:
+            if not self.socket_close:
+                print(f"Error in recieved data for Client: {client_id}: {e}")
+                self.close_socket()
 
-            if not self.unreliable_send_ack(packet_id1, server_address):
-                print("Retransmission received")
-                self.retransmissions_received += 1
-            self.total_bytes_received += len(modified_message)
+            print(f"Total Bytes Received: {self.total_bytes_received}")
+            print(f"Total Retransmission Received: {self.retransmissions_received}")
 
-        print(f"Total Bytes Received: {self.total_bytes_received}")
-        print(f"Total Retransmission Received: {self.retransmissions_received}")
-
-        self.socket.close()
+    def close_socket(self):
+        if not self.socket_closed:
+            self.socket.close()
+            self.socket_closed = True
 
 
 def main():
     server_name = "localhost"
     server_port = 12000
     ack_probability = 0.2
+    num_clients = 2
+    receivers = []
 
-    client_socket = socket(AF_INET, SOCK_DGRAM)
+    try:
+        for client_id in range (1, num_clients + 1):
+            client_socket = socket(AF_INET, SOCK_DGRAM)
+            message = '1'  # Indicate to the server that the client is ready to receive
+            client_socket.sendto(message.encode(), (server_name, server_port))
 
-    message = '1'  # Indicate to the server that the client is ready to receive
-    client_socket.sendto(message.encode(), (server_name, server_port))
+            receiver = UnreliableReceiver(client_socket, ack_probability, num_clients)
+            receivers.append(receiver)
 
-    receiver = UnreliableReceiver(client_socket, ack_probability)
-    receiver.receive_data()
+            thread = threading.Thread(target=receiver.receive_data, args=(client_id,))
+            thread.start()
 
+        # Wait for all threads to finish
+        for thread in threading.enumerate():
+            if thread != threading.current_thread():
+                thread.join()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+    finally:
+        # Close sockets for all UnreliableReceiver objects
+        for receiver in receivers:
+            receiver.close_socket()
+            
 if __name__ == "__main__":
     main()
