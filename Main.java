0c27c320aa43e152ad79d5abd1308947a9beaf7b
Dana Da Costa <dana.dacosta.001@student.uni.lu>
2024-01-08 20:49:41 +0100

    5

--- Main.java
+++ Main.java
@@ -51,7 +51,7 @@ public class Main {
 
     public static void main(String[] args) {
         if (args.length != 7) {
-            System.out.println("Launching Main");
+            System.out.println("Usage: java main.java <port> <client_number> <size> <server_name> <server_port> <ack_probability> <filename>");
             System.exit(1);
         }
         int port = Integer.parseInt(args[0]);
