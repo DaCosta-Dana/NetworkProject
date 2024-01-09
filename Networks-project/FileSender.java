fffbb6606a040d6a5edaac20f58d85795aace9dd
Dana Da Costa <dana.dacosta.001@student.uni.lu>
2024-01-09 15:05:46 +0100

    2

--- Networks-project/FileSender.java
+++ Networks-project/FileSender.java
@@ -151,6 +151,7 @@ class FileSender {
                     return;
                 }
             }
+            
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
